bcf9961cd42d3be628686cdabc028a901d310922
Mohammad Milad Naseri <mmnaseri@programmer.net>
2017-05-29 20:13:13 -0700

    code consistency across all implementations

--- src/main/java/com/mmnaseri/projects/utils/diff/change/impl/BruteForceChangeCalculator.java
+++ src/main/java/com/mmnaseri/projects/utils/diff/change/impl/BruteForceChangeCalculator.java
@@ -1,7 +1,6 @@
 package com.mmnaseri.projects.utils.diff.change.impl;
 
 import com.mmnaseri.projects.utils.diff.change.ChangeCalculationConfiguration;
-import com.mmnaseri.projects.utils.diff.change.ChangeCalculator;
 import com.mmnaseri.projects.utils.diff.domain.Change;
 import com.mmnaseri.projects.utils.diff.domain.Comparison;
 import com.mmnaseri.projects.utils.diff.domain.Item;
@@ -9,7 +8,6 @@ import com.mmnaseri.projects.utils.diff.domain.impl.ImmutableChange;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,57 +15,46 @@ import java.util.stream.Collectors;
  * @author Mohammad Milad Naseri (mmnaseri@programmer.net)
  * @since 1.0 (5/29/17, 4:12 PM)
  */
-public class BruteForceChangeCalculator implements ChangeCalculator {
+public class BruteForceChangeCalculator extends AbstractChangeCalculator {
 
     @Override
     public <V, E extends Item<V>> List<Change<V, E>> calculate(ChangeCalculationConfiguration<V, E> configuration, List<E> source, List<E> target) {
-        return calculate(configuration, source, target, 0, 0);
+        return calculate(configuration, source, target, new Address(0, 0));
     }
 
-    private <V, E extends Item<V>> List<Change<V, E>> calculate(ChangeCalculationConfiguration<V, E> configuration, List<E> source, List<E> target, int sourceCursor, int targetCursor) {
-        if (sourceCursor >= source.size() && targetCursor >= target.size()) {
-            // If the cursors have both fallen over, there aren't any more changes to be done
-            return new LinkedList<>();
-        } else if (sourceCursor >= source.size() && targetCursor < target.size()) {
-            final List<Change<V, E>> changes = new LinkedList<>();
-            // If the source has been evaluated but the target still remains, we can only insert items from the target
-            for (int i = targetCursor; i < target.size(); i++) {
-                E item = target.get(i);
-                changes.add(ImmutableChange.insert(item, targetCursor ++));
-            }
-            return changes;
-        } else if (sourceCursor < source.size() && targetCursor >= target.size()) {
-            final List<Change<V, E>> changes = new LinkedList<>();
-            // If the target has been evaluated but the source still remains, we can only delete from the source
-            for (int i = sourceCursor; i < source.size(); i++) {
-                changes.add(ImmutableChange.delete(targetCursor));
-            }
-            return changes;
+    private <V, E extends Item<V>> List<Change<V, E>> calculate(ChangeCalculationConfiguration<V, E> configuration, List<E> source, List<E> target, Address address) {
+        final List<Change<V, E>> baseCaseResult = handleBaseCases(source, target, address);
+        if (baseCaseResult != null) {
+            return baseCaseResult;
         }
         // At this point, both lists have items to go through
         // First, let's see how the items compare against each other
-        final Comparison comparison = configuration.getItemComparator().compare(source.get(sourceCursor), target.get(targetCursor));
+        final Comparison comparison = configuration.getItemComparator().compare(source.get(address.getSourceCursor()), target.get(address.getTargetCursor()));
         final List<List<Change<V, E>>> possibilities = new ArrayList<>();
         if (Comparison.EDITED.equals(comparison)) {
             // It might be possible to edit one item into the other and take it from there
-            List<Change<V, E>> possibility = calculate(configuration, source, target, sourceCursor + 1, targetCursor + 1);
-            possibility.add(0, ImmutableChange.modify(target.get(targetCursor), targetCursor));
+            final Address next = new Address(address.getSourceCursor() + 1, address.getTargetCursor() + 1);
+            List<Change<V, E>> possibility = calculate(configuration, source, target, next);
+            possibility.add(0, ImmutableChange.modify(target.get(address.getTargetCursor()), address.getTargetCursor()));
             possibilities.add(possibility);
         }
         if (Comparison.EQUAL.equals(comparison)) {
             // If they are equal at this point, maybe we can keep them like that
-            possibilities.add(calculate(configuration, source, target, sourceCursor + 1, targetCursor + 1));
+            final Address next = new Address(address.getSourceCursor() + 1, address.getTargetCursor() + 1);
+            possibilities.add(calculate(configuration, source, target, next));
         }
         // Now, let's see what happens if we try to delete an item from the source
         {
-            List<Change<V, E>> possibility = calculate(configuration, source, target, sourceCursor + 1, targetCursor);
-            possibility.add(0, ImmutableChange.delete(targetCursor));
+            final Address next = new Address(address.getSourceCursor() + 1, address.getTargetCursor());
+            List<Change<V, E>> possibility = calculate(configuration, source, target, next);
+            possibility.add(0, ImmutableChange.delete(address.getTargetCursor()));
             possibilities.add(possibility);
         }
         // After that, we are going to insert the current item from the target and see what happens
         {
-            List<Change<V, E>> possibility = calculate(configuration, source, target, sourceCursor, targetCursor + 1);
-            possibility.add(0, ImmutableChange.insert(target.get(targetCursor), targetCursor));
+            final Address next = new Address(address.getSourceCursor(), address.getTargetCursor() + 1);
+            List<Change<V, E>> possibility = calculate(configuration, source, target, next);
+            possibility.add(0, ImmutableChange.insert(target.get(address.getTargetCursor()), address.getTargetCursor()));
             possibilities.add(possibility);
         }
         // Now, let's find the least costly possibility
